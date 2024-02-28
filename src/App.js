37d86b1b941b08966a329d907b276e514788ca97
Benson <Benson.quah.sg@gmail.com>
2024-02-28 10:29:11 +0800

    adjust

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-import React, { useRef } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { FaBars, FaTimes } from "react-icons/fa";
-import AboutPage from "./Pages/LandingPage/LandingPage";
-import ContactPage from "./Pages/ContactMe/ContactPage";
-import EducationPage from "./Pages/Education/EducationPage";
-import JobExperience from "./Pages/WorkExperience/JobExperience";
-import VolunteerExperience from "./Pages/VolunteerExperience/VolunteerExperience";
-import AwardsPage from "./Pages/Awards/CommendationsPage";
-import ProjectPage from "./Pages/Projects/Projects";
-import Footer from "./Component/Footer/Footer";
-import PageNotFound from "./Component/ErrorHandling/PageNotFound";
-import "./Component/Navbar.css";
 function App() {
   const navRef = useRef();
 
@@ -24,37 +11,25 @@ function App() {
         <header className="header-nav">
           <h3>Benson Quah</h3>
           <nav ref={navRef}>
-            <NavLink to="/Benson_WebPortfolio/" onClick={toggleNavbar}>
+            <NavLink to="/" onClick={toggleNavbar}>
               Home
             </NavLink>
-            <NavLink
-              to="/Benson_WebPortfolio/EducationJourney"
-              onClick={toggleNavbar}
-            >
+            <NavLink to="/EducationJourney" onClick={toggleNavbar}>
               Education
             </NavLink>
-            <NavLink
-              to="/Benson_WebPortfolio/WorkExperience"
-              onClick={toggleNavbar}
-            >
+            <NavLink to="/WorkExperience" onClick={toggleNavbar}>
               Work Experience
             </NavLink>
-            <NavLink to="/Benson_WebPortfolio/Projects" onClick={toggleNavbar}>
+            <NavLink to="/Projects" onClick={toggleNavbar}>
               Projects
             </NavLink>
-            <NavLink
-              to="/Benson_WebPortfolio/VolunteerExperience"
-              onClick={toggleNavbar}
-            >
+            <NavLink to="/VolunteerExperience" onClick={toggleNavbar}>
               Volunteer Experience
             </NavLink>
-            <NavLink
-              to="/Benson_WebPortfolio/AwardsReceived"
-              onClick={toggleNavbar}
-            >
+            <NavLink to="/AwardsReceived" onClick={toggleNavbar}>
               Awards
             </NavLink>
-            <NavLink to="/Benson_WebPortfolio/ContactMe" onClick={toggleNavbar}>
+            <NavLink to="/ContactMe" onClick={toggleNavbar}>
               Connect with Me
             </NavLink>
             <button className="nav-btn nav-close-btn" onClick={toggleNavbar}>
@@ -72,27 +47,17 @@ function App() {
   );
 }
 
-function NavLink({ to, onClick, children }) {
-  return (
-    <Link to={to} onClick={onClick}>
-      {children}
-    </Link>
-  );
-}
-
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/Benson-WebPortfolio" element={<AboutPage />} />
+      <Route path="/" element={<AboutPage />} />
       <Route path="/EducationJourney" element={<EducationPage />} />
       <Route path="/WorkExperience" element={<JobExperience />} />
       <Route path="/Projects" element={<ProjectPage />} />
       <Route path="/VolunteerExperience" element={<VolunteerExperience />} />
-      <Route path="/AwardsReceived*" element={<AwardsPage />} />
+      <Route path="/AwardsReceived" element={<AwardsPage />} />
       <Route path="/ContactMe" element={<ContactPage />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
-
-export default App;
